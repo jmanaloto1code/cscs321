2b842a7d16824703e236654196e388fe5dfb162e
joseph <josephmanaloto1@gmail.com>
2026-03-03 17:41:43 +0000

    Lab #7 first python version

---
+++ jmanaloto7.py
@@ -0,0 +1,25 @@
+#Joseph Manaloto
+#lab #7
+
+x = 75
+y = 145.50
+z = "Simple and Clean"
+
+if x > 70:
+    print("X is greater than 70")
+elif x< 80:
+    print("X is less than  80 and the value of x is ",x)
+else:
+    print("X is ", x, "which is equal to 75")
+
+if y > 60:
+    print("You can buy the game, it will cost $ ",y)
+else:
+    print("You don't have enough money, it cost more than $",y)
+
+if z == "Simple and Clean":
+    print("The next song that will be playing: ",z)
+else:
+    print("This song will be skipped due to no one going up to play. ",z)
+
+
